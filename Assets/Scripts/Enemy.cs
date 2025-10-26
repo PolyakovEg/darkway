546467da437df05cc223c5c60ada8db2d6bc61da
Egor Polyakov <egor.polyakov.06@list.ru>
2025-10-26 18:24:56 +0300

    Матерь зова - тест

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,65 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private Transform _targetTransform;
     private NavMeshAgent agent;
-
+    private float rayMaxDistance = 15f;
+    private Transform[] _wayPoints;
+    float redirectTimer = 0;
+    float redirectFrequency = 20;
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        // TODO: переделать говнокод
+        Radio[] radios = FindObjectsByType<Radio>(FindObjectsSortMode.None);
+        _wayPoints = new Transform[radios.Length];
+
+        for (int i = 0; i < radios.Length; i++)
+        {
+            _wayPoints[i] = radios[i].transform;
+        }
+    }
+
+    private void Start()
+    {
+        SetRandomDestination();
     }
 
     void Update()
     {
-        agent.SetDestination(_targetTransform.position);
+        if (Physics.Raycast(transform.position, _targetTransform.position - transform.position, out RaycastHit hitInfo))
+        {
+            if(hitInfo.transform.CompareTag("Player"))
+            {
+                agent.SetDestination(_targetTransform.position);
+            }
+            else
+            {
+                redirectTimer += Time.deltaTime;
+
+                if (redirectTimer > redirectFrequency)
+                {
+                    redirectTimer = 0;
+                    SetRandomDestination();
+                }
+            }
+        }
+    }
+
+    private void SetRandomDestination()
+    {
+        agent.SetDestination(_wayPoints[Random.Range(0, _wayPoints.Length)].position);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
